d7289daef64fedf4496598514b05bc6021c149fb
Michael Neumann <mneumann@ntecs.de>
2020-04-24 22:50:36 +0200

    Add htons and ntons

--- src/lib_c/x86_64-dragonfly/c/arpa/inet.cr
+++ src/lib_c/x86_64-dragonfly/c/arpa/inet.cr
@@ -2,6 +2,8 @@ require "../netinet/in"
 require "../stdint"
 
 lib LibC
+  fun htons(x0 : UInt16T) : UInt16T = Intrinsics.bswap16(x0)
+  fun ntohs(x0 : UInt16T) : UInt16T = Intrinsics.bswap16(x0)
   fun inet_ntop(x0 : Int, x1 : Void*, x2 : Char*, x3 : SocklenT) : Char*
   fun inet_pton(x0 : Int, x1 : Char*, x2 : Void*) : Int
 end
