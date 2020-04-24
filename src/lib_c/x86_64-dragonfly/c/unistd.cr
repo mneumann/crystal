f3ca7d1c06b5a074baa69ef17b3467c002262e35
Michael Neumann <mneumann@ntecs.de>
2020-04-24 23:13:31 +0200

    DragonFly does not have fdatasync()

--- src/lib_c/x86_64-dragonfly/c/unistd.cr
+++ src/lib_c/x86_64-dragonfly/c/unistd.cr
@@ -16,7 +16,6 @@ lib LibC
   fun dup2(x0 : Int, x1 : Int) : Int
   fun _exit(x0 : Int) : NoReturn
   fun execvp(x0 : Char*, x1 : Char**) : Int
-  fun fdatasync(fd : Int) : Int
   @[ReturnsTwice]
   fun fork : PidT
   fun fsync(fd : Int) : Int
